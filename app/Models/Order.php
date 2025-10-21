dd865430ca0253ee299de944d970b689e792e480
morteza <mortezaghube@gmail.com>
2025-10-21 17:28:32 +0300

    feat(order): add olderThan() helper to Order model

---
+++ app/Models/Order.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+/**
+ * @property Carbon $created_at
+ */
+
+class Order extends Model
+{
+    protected $fillable =
+    [
+        'user_id',
+        'status',
+        'price',
+    ];
+
+    protected $casts =
+    [
+        'status' => OrderStatus::class,
+    ];
+
+    public function markAsCanceled()
+    {
+        $this->update(['status' => OrderStatus::CANCELED]);
+    }
+
+    public function olderThan($minutes): bool
+    {
+        return ($this->created_at->diffInMinutes() > $minutes);
+    }
+}
