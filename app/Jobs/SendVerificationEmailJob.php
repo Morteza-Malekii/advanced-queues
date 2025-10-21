47f66a5affa111cacb46db26bb625ce39f4c8215
morteza <mortezaghube@gmail.com>
2025-10-21 17:34:58 +0300

    fix(jobs):fix typo in sendverificationemailjob handle method

--- app/Jobs/SendVerificationEmailJob.php
+++ app/Jobs/SendVerificationEmailJob.php
@@ -6,21 +6,22 @@ use App\Mail\NewVerificationEmail;
 use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 
 class SendVerificationEmailJob implements ShouldQueue
 {
-    use Queueable;
+    use Queueable, SerializesModels;
 
-    public $tries = 5;
-    public $backoff = [60,60*10,60*60];
+    // public $tries = 5;
+    // public $backoff = [60,60*10,60*60];
 
     /**
      * Create a new job instance.
      */
     public function __construct(public User $user)
     {
-        $this->onQueue('sending-verification');
+        // $this->onQueue('sending-verification');
     }
 
     /**
