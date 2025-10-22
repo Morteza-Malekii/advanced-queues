73b70244ce5a783f9b3f26d6a65fac01db8975fb
morteza <mortezaghube@gmail.com>
2025-10-22 23:41:39 +0300

    feat(WebHook):add failed() to sendwebhook job

---
+++ app/Jobs/SendWebHook.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class SendWebHook implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public string $url, public $data, public Order $order)
+    {
+        //
+    }
+    public $tries = 3;
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $response = Http::post($this->url, $this->data);
+        if($response->failed())
+        {
+            $this->release(now()->addSecond(10));
+        }else
+        {
+            $this->order->markAsComleted();
+        }
+    }
+
+    // public function retryUntil()
+    // {
+
+    // }
+
+    public function failed()
+    {
+        Log::info('do somthing after send webhook failed');
+        $this->order->markAsCanceled();
+    }
+}
