767da60446fac091396aa80786d6e19403058298
morteza <mortezaghube@gmail.com>
2025-10-22 23:43:42 +0300

    feat(dispatch):dispatch sendwebhook in checkoutController

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Enums\OrderStatus;
 use App\Jobs\MonitorPendingOrder;
+use App\Jobs\SendWebHook;
 use App\Models\Order;
 
 class CheckoutController extends Controller
@@ -16,7 +17,12 @@ class CheckoutController extends Controller
             'price'=>1000000
         ]);
 
-        MonitorPendingOrder::dispatch($order,)->delay(20);
+        // MonitorPendingOrder::dispatch($order,)->delay(20);
 
+
+        SendWebHook::dispatch('https://webhook.site/f2c0aa32-a9e3-4f02-ae1e-0b4575b243ec', [
+            'price' => 1000,
+            'name'=> 'morteza maleki'
+        ],$order);
     }
 }
