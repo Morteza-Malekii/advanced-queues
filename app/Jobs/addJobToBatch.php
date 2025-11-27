859a16f96e3509b1f6d662fe7ad6287413d41996
morteza <mortezaghube@gmail.com>
2025-11-27 17:29:40 +0300

    feat(batch):Implement how can add job to runing batches

---
+++ app/Jobs/addJobToBatch.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+
+class addJobToBatch implements ShouldQueue
+{
+    use Queueable, Batchable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->batch()->add(function(){
+            Log::info('this job add to campain');
+        });
+    }
+}
